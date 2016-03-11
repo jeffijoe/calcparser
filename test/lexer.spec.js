93b106bdd2764086d8ad735f2a7df3313d409b1e
Jeff Hansen <jeffijoe@hotmail.com>
2016-03-11 20:12:41 +0100

    Added test stub

---
+++ test/lexer.spec.js
@@ -0,0 +1,8 @@
+const Lexer = require('../lexer');
+const expect = require('chai').expect;
+
+describe('Lexer', function() {
+  describe('lex', function() {
+    it('returns the correct tokens');
+  });
+});
